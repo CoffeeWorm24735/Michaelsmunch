3ee67753b5e607dad17aba502bb58c34397d8ce9
CoffeeWorm24735 <64645810+CoffeeWorm24735@users.noreply.github.com>
2021-12-06 14:43:38 +0000

    Login
    Login updated

--- munchy/passenger_wsgi.py
+++ munchy/passenger_wsgi.py
@@ -51,6 +51,38 @@ def api():
         
         return "Fail"
 
+@app.route("/<string:name>/")
+def say_hello(name):
+    return f"Hello {name}!"
+  
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        # Check if account exists using MySQL
+        
+        mycursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
+        # Fetch one record and return result
+        account = mycursor.fetchone()
+        # If account exists in accounts table in out database
+        if account:
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            # Redirect to home page
+            return redirect("www.michaelsmunch.com/charlie")
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect username/password!'
+    # Show the login form with message (if any)
+    return render_template('index.html', msg=msg)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
